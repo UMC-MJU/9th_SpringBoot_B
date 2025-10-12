26b2bb76c233d0e0b0cff84b4425d000e579f2e4
twodo0 <dopal0426@gmail.com>
2025-10-12 20:20:19 +0900

    feat: 초기 엔티티 설계 및 DB 연결

---
+++ yeongdo/week04/src/main/java/com/example/umc9th/domain/member/enums/OnboardingStage.java
@@ -0,0 +1,6 @@
+package com.example.umc9th.domain.member.enums;
+
+public enum OnboardingStage {
+    SIGNED_UP, CONSENTED, PROFILE_DONE, PREFERENCE_DONE, DONE
+    // 회원가입 시작, 약관 동의, 프로필 설정, 선호 음식, 전부 완료 !!
+}
