d1609272a17fc96316d5fb2d265943161b977666
Son <thswnsrb467@naver.com>
2025-11-09 16:34:53 +0900

    feat: init

---
+++ joon/6주차/src/main/java/com/example/umc9th/domain/mission/dto/MissionListDto.java
@@ -0,0 +1,16 @@
+package com.example.umc9th.domain.mission.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class MissionListDto {
+    private Long missionId;       // 미션 ID
+    private String storeName;     // 가게 이름
+    private String missionName;   // 미션 이름
+    private int rewardPoint;      // 보상 포인트
+    private int daysLeft;         // 남은 기한 (D-Day)
+}
---
+++ joon/6주차/src/main/java/com/example/umc9th/domain/mission/repository/MemberMissionRepository.java
@@ -0,0 +1,23 @@
+package com.example.umc9th.domain.mission.repository;
+
+import com.example.umc9th.domain.mission.dto.MemberMissionDto;
+import com.example.umc9th.domain.mission.entity.mapping.MemberMission;
+import com.example.umc9th.domain.mission.entity.mapping.MemberMissionId;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MemberMissionRepository extends JpaRepository<MemberMission, MemberMissionId> {
+    // 진행중, 완료한 미션 목록
+    @Query("SELECT new com.example.umc9th.domain.mission.dto.MemberMissionDto(" +
+            "mm.id, m.rewardPoint, s.storeName, m.missionName, mm.isCompleted) " +
+            "FROM MemberMission mm " +
+            "JOIN mm.mission m " +
+            "JOIN m.store s " +
+            "WHERE mm.member.id = :memberId")
+    Page<MemberMissionDto> findByMemberId(@Param("memberId") Long memberId, Pageable pageable);
+}
