b1da157a56971aceaa9583843777bf8bab5a5724
twodo0 <dopal0426@gmail.com>
2025-12-15 14:19:08 +0900

    feat: MemberController에 로그인 구현

--- yeongdo/week10/src/main/java/com/example/umc9th/domain/member/controller/MemberController.java
+++ yeongdo/week10/src/main/java/com/example/umc9th/domain/member/controller/MemberController.java
@@ -4,6 +4,7 @@ import com.example.umc9th.domain.member.code.MemberSuccessCode;
 import com.example.umc9th.domain.member.dto.MemberReqDto;
 import com.example.umc9th.domain.member.dto.MemberResDto;
 import com.example.umc9th.domain.member.service.command.MemberCommandService;
+import com.example.umc9th.domain.member.service.query.MemberQueryService;
 import com.example.umc9th.global.apiPayload.ApiResponse;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
@@ -16,7 +17,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class MemberController {
 
     private final MemberCommandService memberCommandService;
-
+    private final MemberQueryService memberQueryService;
+    // 회원가입
     @PostMapping("/auth/signup")
     public ApiResponse<MemberResDto.JoinDto> signup(
             // 검증 실패 시 -> MethodArgumentNotValidException 발생
@@ -24,4 +26,13 @@ public class MemberController {
     ) {
         return ApiResponse.onSuccess(MemberSuccessCode.Found, memberCommandService.signUp(dto));
     }
+
+    // 로그인
+    @PostMapping("/auth/login")
+    public ApiResponse<MemberResDto.LoginDto> login(
+            @RequestBody @Valid MemberReqDto.LoginDto dto
+    ) {
+        return ApiResponse.onSuccess(MemberSuccessCode.Found, memberQueryService.login(dto));
+    }
+
 }
